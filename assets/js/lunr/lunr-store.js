626209c49391d76348555a34e66ba245dce12e0d
github_czy <1060324818@qq.com>
2025-03-30 21:07:45 +0800

    不使用自动构建了

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "企业编码生成系统",
+        "excerpt":"   项目 tags:   Python   编码系统 excerpt: “采用qrcode与pystrich库开发的编码生成系统”     项目源码   功能描述      生成6位数字防伪编码   生成9位系列产品防伪编码   生成25位混合产品序列号   批量生成EAN-13条形码   批量生成二维码   响应时间&lt;200毫秒   项目特点      支持数据分析功能   支持后续补充生成防伪码   开源项目    ","categories": [],
+        "tags": [],
+        "url": "/2024/09/20/%E4%BC%81%E4%B8%9A%E7%BC%96%E7%A0%81%E7%94%9F%E6%88%90%E7%B3%BB%E7%BB%9F.html",
+        "teaser": null
+      },{
+        "title": "除夕猜灯谜小游戏",
+        "excerpt":"项目链接  ","categories": ["项目"],
+        "tags": ["游戏","Python"],
+        "url": "/%E9%A1%B9%E7%9B%AE/2024/09/29/%E9%99%A4%E5%A4%95%E7%8C%9C%E7%81%AF%E8%B0%9C%E5%B0%8F%E6%B8%B8%E6%88%8F.html",
+        "teaser": null
+      }]
